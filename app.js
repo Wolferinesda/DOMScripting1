26fda45c9cde76d1456fd39521eefb34e205d636
Daniel Halterman <drhalterman1@yahoo.com>
2017-11-14 06:32:00 -0500

    Assignment 8 changes

--- app.js
+++ app.js
@@ -3,11 +3,41 @@ window.onload = function() {
 };
 
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', () => {
-  });
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+       title: newToDoText.value,
+       complete: false
+    });
+    newToDoText.value = '';
+    if (!newToDoText.value) { return; }
+     renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+
+  addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
+ });
+  renderTheUI();
 }
 
 addToDoForm.addEventListener('submit', event => {
