9a15c3d4fa910e8b67588b32a7ca8a91211abe81
Daniel Halterman <drhalterman1@yahoo.com>
2017-11-14 06:37:19 -0500

    Lesson 8 Assignement Changes

--- app.js
+++ app.js
@@ -3,35 +3,65 @@ window.onload = function() {
 };
 
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    toDos.push({
+       title: newToDoText.value,
+       complete: false
+    });
+    newToDoText.value = '';
+    if (!newToDoText.value) { return; }
+     renderTheUI();
+  }
 
-  addToDoForm.addEventListener('submit', () => {
-    event.preventDefault();
-    let title = newToDoText.value;
-    // create a new li
-    let newLi = document.createElement('li');
-    // create a new input
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+
+  addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
+ });
+  renderTheUI();
+}
+
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+     let title = newToDoText.value;
+     // create a new li
+     let newLi = document.createElement('li');
+
+     // create a new input
     let checkbox = document.createElement('input');
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
-    let delButton = document.createElement('button');
-    delButton.textContent = "Delete";
-
-    delButton.addEventListener('click', () => {
-      toDoList.removeChild(newLi);
-    });
-
     // set the title
     newLi.textContent = title;
+
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
-    newLi.appendChild(delButton);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
+
     //empty the input
     newToDoText.value = '';
-  });
+});
 }
