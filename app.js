387d963d6d48062b7c64f4876685945f1506c9fa
Daniel Halterman <drhalterman1@yahoo.com>
2017-11-30 10:04:02 -0500

    Created a functional delete button

--- app.js
+++ app.js
@@ -3,65 +3,55 @@ window.onload = function() {
 };
 
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {return}
+
     toDos.push({
-       title: newToDoText.value,
-       complete: false
+      id: id,
+      title: newToDoText.value,
+      complete: false
     });
+    id++;
     newToDoText.value = '';
-    if (!newToDoText.value) { return; }
-     renderTheUI();
+    renderTheUI();
+  }
+
+  function deleteToDo(id) {
+    toDos = toDos.filter(item => item.id !== id);
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
+     const newLi = document.createElement('li');
+     const checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
+     newLi.textContent = toDo.title;
 
-      newLi.textContent = toDo.title;
+     const removeButton = document.createElement('button');
+     removeButton.textContent = 'Remove To-Do';
+     removeButton.addEventListener('click', event => {
+       deleteToDo(toDo.id);
+       renderTheUI();
+     });
 
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-
-
+     toDoList.appendChild(newLi);
+     newLi.appendChild(checkbox);
+     newLi.appendChild(removeButton);
+   });
+  }
   addToDoForm.addEventListener('submit', event => {
-   event.preventDefault();
-   createNewToDo();
- });
+    event.preventDefault();
+    createNewToDo();
+  });
+
+
   renderTheUI();
-}
-
-addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-     let title = newToDoText.value;
-     // create a new li
-     let newLi = document.createElement('li');
-
-     // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value = '';
-});
 }
